8bc1a6f83edc594b85c43a0a09470cdc3e75c327
freddyj01 <fahrodej01@gmail.com>
2025-12-13 09:02:04 -0500

    LINQ & IEnumerable

---
+++ DELETEME/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DELETEME.Program;
+
+class Program
+{
+    static void Main(string[] Args)
+    {
+        List<int> grades = [12, 21, 6, 15, 30];
+
+        // Find Grades <= 15
+        IEnumerable<int> gradesQuery =
+            from grade in grades
+            where grade <= 15
+            select grade;
+        
+        foreach (int grade in gradesQuery)
+        {
+            System.Console.WriteLine($"Grade below 15 -> {grade}");
+        }
+    }
+}
